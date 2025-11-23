a780b7e7660d61fafcdf204132f52123c572d4e5
Aditya-Mishra799 <adityamishra9124@gmail.com>
2025-11-23 13:02:00 +0530

    modified health endpoint as per docs

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.use(cors({
 }))
 
 app.get('/', (req, res) => res.send('Server is running on port: ' + port))
-app.get("/health", (req, res) => {
+app.get("/healthz", (req, res) => {
     res.status(200).json({
         status: "OK",
         uptime: process.uptime(),                
